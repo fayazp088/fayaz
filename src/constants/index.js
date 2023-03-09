98653c99b23003e04defc96367f4b6c59e3f9623
fayaz <fayazp088@gmail.com>
2023-03-09 17:09:35 +0530

    adding the work experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,8 @@ import {
   jobit,
   tripguide,
   threejs,
+  gravityCompanyLogo,
+  fizeCompanyLogo,
 } from '../assets';
 
 export const navLinks = [
@@ -116,24 +118,11 @@ const technologies = [
 
 const experiences = [
   {
-    title: 'React.js Developer',
-    company_name: 'Starbucks',
-    icon: starbucks,
-    iconBg: '#383E56',
-    date: 'March 2020 - April 2021',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
-    title: 'React Native Developer',
-    company_name: 'Tesla',
-    icon: tesla,
+    title: 'Software Engineer II',
+    company_name: 'Fize',
+    icon: fizeCompanyLogo,
     iconBg: '#E6DEDD',
-    date: 'Jan 2021 - Feb 2022',
+    date: 'Jan 2022 - Current',
     points: [
       'Developing and maintaining web applications using React.js and other related technologies.',
       'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
@@ -142,24 +131,24 @@ const experiences = [
     ],
   },
   {
-    title: 'Web Developer',
-    company_name: 'Shopify',
-    icon: shopify,
-    iconBg: '#383E56',
-    date: 'Jan 2022 - Jan 2023',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
-    title: 'Full stack Developer',
-    company_name: 'Meta',
-    icon: meta,
+    title: 'Software Engineer I',
+    company_name: 'Fize',
+    icon: fizeCompanyLogo,
     iconBg: '#E6DEDD',
-    date: 'Jan 2023 - Present',
+    date: 'Dec 2021 - Jan 2022',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Associate Software Engineer',
+    company_name: 'Gravity Ilabs',
+    icon: gravityCompanyLogo,
+    iconBg: '#E6DEDD',
+    date: 'Nov 2020 - Dec 2021',
     points: [
       'Developing and maintaining web applications using React.js and other related technologies.',
       'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
